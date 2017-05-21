f7cd4f1a55c1c39c2e8dd1c06f3e646fa01f1d07
Charlie Hsu <charlie@twilio.com>
2017-05-20 17:53:54 -0700

    gateway with basic message printing

--- lightshow/gateway.py
+++ lightshow/gateway.py
@@ -1,5 +1,6 @@
 import urllib2
 import json
+import time
 import serial
 import logging
 import logging.config
@@ -35,6 +36,14 @@ LOG_SETTINGS = {
         },
     },
 }
+
+TECTHULU_ENDPOINT = 'http://TecthulhuFF.local/portals/Tecthulhu05.json'
+# can also be status/faction, status/health
+
+USB_ENABLED = False
+USB_ENDPOINT = '/dev/tty.usbmodem1411'
+
+
 logging.config.dictConfig(LOG_SETTINGS)
 
 def main():
@@ -44,16 +53,88 @@ def main():
     logger = logging.getLogger('lightshow')
     running = True
     logger.info("Starting gateway, press C-c to exit")
-    logger.debug("SDLFKJSDLFKJSDF")
-    try:
-        while running:
-        #event loop
-            raise NotImplementedError("Not Implemented")
 
-    except KeyboardInterrupt:
-        logger.info("exiting")
-    except Exception as e:
-        logger.error(str(e))
+    # set up usb
+    if USB_ENABLED:
+        usb = serial.Serial(USB_ENDPOINT)
+
+    # state variables
+    prev_states = []
+
+    while running:
+
+        try:
+
+            # fetch data from tecthulu
+            raw_response = urllib2.urlopen(
+                urllib2.Request(TECTHULU_ENDPOINT)
+            ).read()
+
+            resp = json.loads(raw_response)['externalApiPortal']
+
+            print 'request made'
+
+            # load into previous state, hold up to last 10
+            if len(prev_states) == 0:
+                print 'first state'
+                print resp
+                prev_states.append(resp)    # first time double load
+
+            prev_states.append(resp)
+            prev_states = prev_states[-10:]
+
+            # for now, just get current and previous state
+            current_state = prev_states[-1]
+            before_state = prev_states[-2]
+
+
+
+
+            # health change
+            health_change = 'same'
+
+            if current_state['health'] < before_state['health']:
+                print 'health decreased'
+                health_change = 'weaker'
+
+            elif current_state['health'] > before_state['health']:
+                print 'health increased'
+                health_change = 'stronger'
+
+            if current_state['resonators'] != before_state['resonators']:
+                print 'reso state change'
+
+
+            final_command = ' '.join([
+                current_state['controllingFaction'],
+                str(int(current_state['health'])),
+                health_change
+            ])
+
+            if USB_ENABLED:
+                usb.write(final_command + '\n')
+            else:
+                print final_command
+
+            # sleep between polls
+            time.sleep(3)
+
+
+    # deserialize json
+
+    # return line format
+
+    #event loop
+        #raise NotImplementedError("Not Implemented")
+
+            except KeyboardInterrupt:
+                logger.info("exiting")
+                if USB_ENABLED:
+                    usb.close()
+            except Exception as e:
+                print raw_response
+                logger.error(str(e))
+                continue
 
 if __name__ == "__main__":
     main()
